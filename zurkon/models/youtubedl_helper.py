2afb77bd53756f6321fc7a3ee51bd13fb27db545
Christian Rosheuvel <chris.rosheuvel@gmail.com>
2024-01-21 17:57:04 +0100

    perf: :zap: limit streams to 20 seconds

--- zurkon/models/youtubedl_helper.py
+++ zurkon/models/youtubedl_helper.py
@@ -23,8 +23,34 @@ DEFAULT_PARAMS = {
 }
 
 
+def get_options(start: int, duration: int) -> str:
+    """Set options for ffmpeg, including start of video and duration.
+
+    Args:
+        start (int): start of video in seconds
+        duration (int): duration of video in seconds
+
+    Returns:
+        str: ffmpeg options
+    """
+    if duration >= 20:
+        duration = 20
+    options: str = "-vn"
+    if start > 0:
+        options += f" -ss {start}"
+    if duration > 0:
+        options += f" -t {duration}"
+    return options
+
+
 async def auto_stop_player(duration: float, client: VoiceClient):
-    sleep(duration + 1)
+    """Automatically stop player after duration.
+
+    Args:
+        duration (float): delay in seconds
+        client (VoiceClient): Bot client
+    """
+    sleep(duration + 2)
     if not client.is_connected():
         return
     client.stop()
@@ -39,16 +65,17 @@ class YouTubeDLHelper(PCMVolumeTransformer):
         self.url: str = data.get("url", "")
 
     @classmethod
-    async def from_url(cls, url, *, loop=None, stream=False):
+    async def from_url(cls, url, *, loop=None, stream=False, start: int = 0, duration: int = 20):
         loop = loop or get_event_loop()
         response = await loop.run_in_executor(
             None,
             lambda: YoutubeDL(DEFAULT_PARAMS).extract_info(url, download=False),
         )
-        data: dict[str, str] = (
+        data: dict = (
             response.get("entries", [])[0] if "entries" in response else response
         )
-        filename = data.get("url", "")
-        # options: str = "-vn -ss 7 -t 3"
-        audio = FFmpegPCMAudio(filename, options="-vn")  # type ignore
+        filename: str = data.get("url", "")
+        duration: float = data.get("duration", 0) if duration > data.get("duration", 0) else duration
+        options: str = get_options(start, duration)
+        audio = FFmpegPCMAudio(filename, options=options)  # type ignore
         return cls(audio, data=data)  # type ignore
--- zurkon/models/zurkon_controller.py
+++ zurkon/models/zurkon_controller.py
@@ -1,4 +1,4 @@
-from discord.ext.commands import Cog, CommandError, command
+from discord.ext.commands import Cog, CommandError, command, Context
 from zurkon.models.youtubedl_helper import YouTubeDLHelper, auto_stop_player
 
 
@@ -7,7 +7,7 @@ class ZurkonController(Cog):
         self.bot = bot
 
     @command()
-    async def play(self, ctx, *, url):
+    async def play(self, ctx: Context, *, url: str):
         """Streams from a url (same as yt, but doesn't predownload)"""
         try:
             async with ctx.typing():
@@ -16,14 +16,15 @@ class ZurkonController(Cog):
                     player, after=lambda e: print(f"Player error: {e}") if e else None
                 )
             await ctx.send(f"Now playing: {player.title}")
-            await auto_stop_player(player.data["duration"], ctx.voice_client)
+            duration: float = player.data["duration"] if player.data["duration"] < 20 else 20
+            await auto_stop_player(duration, ctx.voice_client)
         except Exception as e:
             print(e)
             if ctx.voice_client is not None:
                 await ctx.voice_client.disconnect()
 
     @command()
-    async def volume(self, ctx, volume: int):
+    async def volume(self, ctx: Context, volume: int):
         """Changes the player's volume"""
 
         if ctx.voice_client is None:
@@ -33,18 +34,20 @@ class ZurkonController(Cog):
         await ctx.send(f"Changed volume to {volume}%")
 
     @command()
-    async def stop(self, ctx):
+    async def stop(self, ctx: Context):
         """Stops and disconnects the bot from voice"""
 
         await ctx.voice_client.disconnect()
 
     @play.before_invoke
-    async def ensure_voice(self, ctx):
-        if ctx.voice_client is None:
-            if ctx.author.voice:
-                await ctx.author.voice.channel.connect()
-            else:
-                await ctx.send("You are not connected to a voice channel.")
-                raise CommandError("Author not connected to a voice channel.")
-        elif ctx.voice_client.is_playing():
-            ctx.voice_client.stop()
+    async def ensure_voice(self, ctx: Context):
+        voice_client = ctx.voice_client
+        author_voice = ctx.author.voice
+
+        if not author_voice:
+            await ctx.send("You are not connected to a voice channel.")
+            raise CommandError("Author not connected to a voice channel.")
+        if voice_client is None:
+            await author_voice.channel.connect()
+        elif voice_client.is_playing():
+            voice_client.stop()
